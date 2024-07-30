153c0416f8d22351d1beeb6de88b91fc2b92e67a
tobin-ulbricht <tobin.ulbricht@gmail.com>
2024-07-30 11:55:45 +0200

    Vercel Test

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 
 ## Todo
 
-- [ ] Make it deploy
+- [x] Make it deploy
 - [ ] Scaffold basic ui with mock data
 - [ ] Actually set up a database (vercel postgres)
 - [ ] Attach database to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Link from "next/link";
+const mockUrls = [
+    "https://utfs.io/f/a2751cfb-c3d9-4f25-9f8f-2adb876bc030-x3togj.jpg",
+    "https://utfs.io/f/7ade1d1b-df11-47ce-a8da-47e91d2e7988-bksbt9.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello testspace
+    <main className="">
+        <div className="flex flex-wrap gap-4">{
+            mockImages.map((image) => (
+                <div key={image.id} className="w-48">
+                    <img src={image.url} alt="text"/>
+                </div>
+            ))
+        }</div>
     </main>
   );
 }
