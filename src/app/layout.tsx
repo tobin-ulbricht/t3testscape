2814ea09a4dd157e8e1ba06380498ec3954223b6
tobin-ulbricht <tobin.ulbricht@gmail.com>
2024-08-02 11:48:57 +0200

    turbo and supermaven

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,9 @@ export const metadata: Metadata = {
 
 function TopNav() {
     return (
-        <nav className="flex w-full items-center justify-between">
+        <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
             <div>Gallery</div>
+            <div>Sign In</div>
         </nav>
     )
 }
