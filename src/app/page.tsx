761163886bd3e8daa26edbf0e33f67bc49e61826
tobin-ulbricht <tobin.ulbricht@gmail.com>
2024-08-02 15:28:20 +0200

    working database and the rest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,18 @@
-import * as constants from "constants";
 import {db} from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-    "https://utfs.io/f/a2751cfb-c3d9-4f25-9f8f-2adb876bc030-x3togj.jpg",
-    "https://utfs.io/f/7ade1d1b-df11-47ce-a8da-47e91d2e7988-bksbt9.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-}));
-
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
-    console.log(posts);
+    const images = await db.query.images.findMany({
+        orderBy: (model, {desc}) => desc(model.id),
+    });
 
   return (
     <main className="">
         <div className="flex flex-wrap gap-4">
-            {posts.map((post) => (
-                <div key={post.id} className="w-48">
-                    <p>{post.name}</p>
-                </div>
-            ) )}
-            { mockImages.map((image) => (
+            { images.map((image) => (
                 <div key={image.id} className="w-48">
-                    <img src={image.url} alt="text"/>
+                    <img src={image.url} alt={image.url}/>
                 </div>
             ))}
         </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import {sql} from "drizzle-orm";
+import {index, pgTableCreator, serial, timestamp, varchar,} from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,19 +12,20 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3sandbox_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+export const images = createTable(
+    "image",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name", {length: 256}).notNull(),
+        url: varchar("url", {length: 256}).notNull(),
+        createdAt: timestamp("created_at", {withTimezone: true})
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updated_at", {withTimezone: true}).$onUpdate(
+            () => new Date()
+        ),
+    },
+    (example) => ({
+        nameIndex: index("name_idx").on(example.name),
+    })
 );
