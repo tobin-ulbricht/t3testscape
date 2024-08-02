35901973d8e95d2519a0711ed9df0c6bb1de5d88
tobin-ulbricht <tobin.ulbricht@gmail.com>
2024-08-02 14:26:35 +0200

    database changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import * as constants from "constants";
+import {db} from "~/server/db";
+
 const mockUrls = [
     "https://utfs.io/f/a2751cfb-c3d9-4f25-9f8f-2adb876bc030-x3togj.jpg",
     "https://utfs.io/f/7ade1d1b-df11-47ce-a8da-47e91d2e7988-bksbt9.jpg",
@@ -8,16 +11,24 @@ const mockImages = mockUrls.map((url, index) => ({
     url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany();
+    console.log(posts);
+
   return (
     <main className="">
-        <div className="flex flex-wrap gap-4">{
-            mockImages.map((image) => (
+        <div className="flex flex-wrap gap-4">
+            {posts.map((post) => (
+                <div key={post.id} className="w-48">
+                    <p>{post.name}</p>
+                </div>
+            ) )}
+            { mockImages.map((image) => (
                 <div key={image.id} className="w-48">
                     <img src={image.url} alt="text"/>
                 </div>
-            ))
-        }</div>
+            ))}
+        </div>
     </main>
   );
 }
